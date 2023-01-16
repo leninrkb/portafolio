d37a5b0be59aeb676090af72f0b236ba28244ca1
leninrkb <rkb.lenin@gmail.com>
2023-01-15 20:33:58 -0500

    Update App.vue

--- portafolio/src/App.vue
+++ portafolio/src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <v-app>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click="onClick"
+      >Toggle Theme</v-btn>
+    </v-app-bar>
+
     <v-main>
-      <router-view/>
+      <v-container>Content area</v-container>
     </v-main>
   </v-app>
+
+  <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-parallax>
+  
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const theme = ref('light')
+
+  function onClick () {
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
+  }
+</script>
+
 
 <script>
 
