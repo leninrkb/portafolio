a67aa38079561990d1ad132a993b7866e9409c6a
leninrkb <rkb.lenin@gmail.com>
2023-01-16 23:35:25 -0500

    slider para mostrar mis proyectos

--- portafolio/src/App.vue
+++ portafolio/src/App.vue
@@ -8,13 +8,31 @@
 
         <v-btn icon="mdi-dots-vertical"></v-btn>
       </template>
-      <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="onClick">Cambia el tema</v-btn>
+      <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" @click="onClick">Cambia el
+        tema</v-btn>
     </v-app-bar>
 
     <v-main>
       <!-- <v-container> -->
       <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
-        asdasda
+        <div id="contenido">
+          <div id="slider">
+            <v-carousel show-arrows="hover">
+              <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+
+              <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+
+              <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+            </v-carousel>
+          </div>
+          <div id="info">
+            <v-card title="capturar casco" text="..." variant="tonal">
+              <v-card-actions>
+                <v-btn>Click me</v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
+        </div>
       </v-parallax>
       <!-- </v-container> -->
     </v-main>
