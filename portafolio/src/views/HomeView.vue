59b267e77b9c540b501b2180f2fa760e8eda0442
leninrkb <rkb.lenin@gmail.com>
2023-01-22 01:28:48 -0500

    manejo homeview y router

--- portafolio/src/views/HomeView.vue
+++ portafolio/src/views/HomeView.vue
@@ -1,18 +1,115 @@
 <template>
-  <HelloWorld />
+  <v-app :theme="theme">
+    <!-- <div data-aos="fade-down" data-aos-duration="2000">
+      <v-app-bar :elevation="2" rounded>
+        <template v-slot:append>
+          <v-btn icon="mdi-heart"></v-btn>
+
+          <v-btn icon="mdi-magnify"></v-btn>
+
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+        <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          @click="cambiarTema()"></v-btn>
+      </v-app-bar>
+    </div> -->
+
+    <div data-aos="fade-up" data-aos-duration="2000">
+      <v-main>
+        <v-parallax src="../assets/img/fondo3.png">
+          <Logo></Logo>
+          <div class="espacio-abajo">
+            <InfoPersonal></InfoPersonal>
+          </div>
+
+          <div class="espacio-abajo espacios-laterales">
+            <Carousel></Carousel>
+          </div>
+
+          <div class="espacio-abajo espacios-laterales">
+            <HabilidadesDomina></HabilidadesDomina>
+          </div>
+
+          <div class="espacio-abajo espacios-laterales">
+            <Experiencia></Experiencia>
+          </div>
+
+          <div class="espacio-abajo espacios-laterales">
+            <Contacto></Contacto>
+          </div>
+        </v-parallax>
+      </v-main>
+    </div>
+    <div>
+      <Footer></Footer>
+    </div>
+  </v-app>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
   },
 });
 </script>
+
+<script setup >
+import { ref } from 'vue'
+import Footer from '@/components/Footer.vue'
+import Carousel from '@/components/Carousel.vue';
+import InfoPersonal from '@/components/InfoPersonal.vue';
+import HabilidadesDomina from '@/components/HabilidadesDomina.vue'
+import Experiencia from '@/components/Experiencia.vue'
+import Contacto from '@/components/Contacto.vue'
+import Logo from '@/components/Logo.vue';
+import { onMounted } from "vue";
+import AOS from "aos";
+import { magicMouse } from 'magicmouse.js'
+
+onMounted(() => {
+  AOS.init();
+})
+
+const theme = ref('dark');
+const options = {
+  "cursorOuter": "circle-basic",
+  "hoverEffect": "circle-move",
+  "hoverItemMove": false,
+  "defaultCursor": false,
+  "outerWidth": 30,
+  "outerHeight": 30
+};
+magicMouse(options);
+
+function cambiarTema() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+
+.margen {
+  padding: 10px;
+}
+
+.espacio-abajo {
+  margin-bottom: 20px;
+}
+
+.espacios-laterales {
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.espacios-laterales-medio {
+  margin-left: 50px;
+  margin-right: 50px;
+}
+</style>
