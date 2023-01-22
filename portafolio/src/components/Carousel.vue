279776849a7658781832458f76a0228b43579427
leninrkb <rkb.lenin@gmail.com>
2023-01-22 01:12:25 -0500

    update carousel

--- portafolio/src/components/Carousel.vue
+++ portafolio/src/components/Carousel.vue
@@ -3,7 +3,13 @@
         <v-card class="blur" title="Mis proyectos" variant="tonal"></v-card>
         <v-card variant="tonal">
             <v-carousel show-arrows="hover" interval="2500" cycle>
-                <v-carousel-item v-for="img in images" :src="img.src" cover></v-carousel-item>
+                <v-carousel-item v-for="img in images" :src="img.src" cover>
+                    <div class="d-flex fill-height justify-center align-center">
+                        <div class="text-h2">
+                            {{ img.title }}
+                        </div>
+                    </div>
+                </v-carousel-item>
             </v-carousel>
         </v-card>
     </div>
@@ -15,22 +21,28 @@ export default {
         return {
             images: [
                 {
-                    src:require('../assets/img/img_carousel/albedo.jpg')
+                    src: require('../assets/img/img_carousel/albedo.jpg')
+                    , title: 'RIKO'
                 }
-                ,{
-                    src:require('../assets/img/img_carousel/lala.png')
+                , {
+                    src: require('../assets/img/img_carousel/lala.png')
+                    , title: 'cosita'
                 }
-                ,{
-                    src:require('../assets/img/img_carousel/naberalGamma.png')
+                , {
+                    src: require('../assets/img/img_carousel/naberalGamma.png')
+                    , title: 'cosa bien hecha'
                 }
-                ,{
-                    src:require('../assets/img/img_carousel/natsumi.png')
+                , {
+                    src: require('../assets/img/img_carousel/natsumi.png')
+                    , title: 'cosa hermosa'
                 }
-                ,{
-                    src:require('../assets/img/img_carousel/thoka.png')
+                , {
+                    src: require('../assets/img/img_carousel/thoka.png')
+                    , title: 'awww <3'
                 }
-                ,{
-                    src:require('../assets/img/img_carousel/to4.jpg')
+                , {
+                    src: require('../assets/img/img_carousel/to4.jpg')
+                    , title: 'mi amor'
                 }
             ]
         }
