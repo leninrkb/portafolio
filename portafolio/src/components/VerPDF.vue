203d01c8b6ef37a3b34d86027c437278636a04ba
Lenin Acosta <rkb.lenin@gmail.com>
2023-02-07 10:04:14 -0500

    update

--- portafolio/src/components/VerPDF.vue
+++ portafolio/src/components/VerPDF.vue
@@ -1,16 +1,7 @@
 <template>
     <div v-if="ver">
-        aqui va el pdf
-        <!-- <embed src="~/assets/docs/CV_lenin.pdf" type="application/pdf" width="400px" > -->
-        <!-- <iframe src="../assets/docs/CV_lenin.pdf" width="100%" height="500"></iframe> -->
-        <!-- <iframe id="fred" style="border:1px solid #666CCC" title="PDF in an i-Frame" src="../assets/docs/CV_lenin.pdf" frameborder="1" scrolling="auto" width="100%" height="600px" ></iframe> -->
-        <!-- <VuePdfReader url="../assets/docs/CV_lenin.pdf"></VuePdfReader> -->
-        <!-- <object class="pdfview" type="application/pdf" data="../../src/assets/docs/CV_lenin.pdf"></object> -->
-        <!-- <iframe src="https://drive.google.com/file/d/<file_id>/preview" width="100%" height="500"></iframe> -->
-        <iframe src="https://drive.google.com/file/d/1xGpauDhY1sJ82N1eLpp9W2oYF0f54Nai/preview?format=pdf" width="100%" height="500"></iframe>
-
+        <iframe class="pdfview" :src="url" width="100%" height="500"></iframe>
     </div>
-
 </template>
 <script>
 // AIzaSyCoD65xnCh7VjZrWzCkp1EFcTUD36KW8DU google api key
@@ -18,9 +9,7 @@ export default {
     name: 'VerPDF'
     , data() {
         return {
-            // ruta:require("@/assets/docs/CV_lenin.pdf")
-            dir:''
-            
+            url:'https://drive.google.com/file/d/1xGpauDhY1sJ82N1eLpp9W2oYF0f54Nai/preview?format=pdf'
         }
     }
     , props: {
@@ -33,11 +22,12 @@ export default {
     /* Centrado */
     margin: auto;
     display: block;
+    margin-bottom: 5px;
     /* Tamaño */
-    width: 850px;
-    height: 100vh;
+    /* width: 850px; */
+    /* height: 100vh; */
     /* Mejorar aspecto */
-    border-radius: 20px;
+    border-radius: 10px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
