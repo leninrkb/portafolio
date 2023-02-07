f7608395ec563a45ba7b42cafaca85306c0b0d9e
Lenin Acosta <rkb.lenin@gmail.com>
2023-02-07 10:51:02 -0500

    enalces socialmedia

--- portafolio/src/components/Footer.vue
+++ portafolio/src/components/Footer.vue
@@ -2,12 +2,11 @@
         <v-footer class="d-flex flex-column">
             <div class="bg-teal d-flex w-100 align-center px-4">
                 <strong>Mis redes sociales!</strong>
-
                 <v-spacer></v-spacer>
-
-                <v-btn v-for="icon in icons" :key="icon" class="mx-4" :icon="icon" variant="plain" size="small"></v-btn>
+                <v-btn v-for="icon, index in icons" :key="icon" :href="enlaces[index]" class="mx-4" :icon="icon" variant="plain" size="small">
+                    
+                </v-btn>
             </div>
-
             <div class="px-4 py-2 bg-black text-center w-100">
                 {{ new Date().getFullYear() }} — <strong>LeninRKB</strong>
             </div>
@@ -19,11 +18,15 @@ export default {
     name: 'Footer',
     data: () => ({
         icons: [
-            'mdi-facebook',
-            'mdi-twitter',
+            'mdi-github',
             'mdi-linkedin',
-            'mdi-instagram',
+            'mdi-facebook',
         ],
+        enlaces:[
+            'https://github.com/leninrkb',
+            'www.linkedin.com/in/lenin-acosta',
+            'https://www.facebook.com/lenin.acosta.56829',
+        ]
     }),
 }
 </script>
