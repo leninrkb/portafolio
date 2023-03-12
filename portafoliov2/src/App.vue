a3106b2f52fee9e23b32e3b31ba996de928bd7d5
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-12 15:53:49 -0500

    fondo para el body

--- portafoliov2/src/App.vue
+++ portafoliov2/src/App.vue
@@ -19,7 +19,10 @@ export default{
 
 <style>
 body{
-  background-color: dimgrey;
+  background-image: repeating-linear-gradient(135deg, rgb(0,0,0) 0px, rgb(0,0,0) 10px,transparent 10px, transparent 11px),repeating-linear-gradient(22.5deg, rgb(0,0,0) 0px, rgb(0,0,0) 10px,transparent 10px, transparent 11px),linear-gradient(90deg, hsl(194,74%,56%),hsl(266,74%,56%),hsl(338,74%,56%),hsl(50,74%,56%),hsl(122,74%,56%));
+  /* background-image: linear-gradient(45deg, rgb(33,33,33) 0%,transparent 73%),repeating-linear-gradient(90deg, rgba(127, 127, 127,0.1) 0px, rgba(127, 127, 127,0.1) 1px,transparent 1px, transparent 17px),repeating-linear-gradient(0deg, rgba(127, 127, 127,0.1) 0px, rgba(127, 127, 127,0.1) 1px,transparent 1px, transparent 17px),linear-gradient(0deg, rgb(33,33,33),rgb(33,33,33)); */
 }
+
+
 </style>
 
