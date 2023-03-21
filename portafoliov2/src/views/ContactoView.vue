60bfae50b8e7800b6961415305201ea81d223147
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-20 21:25:22 -0500

    contacto card

--- portafoliov2/src/views/ContactoView.vue
+++ portafoliov2/src/views/ContactoView.vue
@@ -1,19 +1,35 @@
 <template>
     <div class="mx-3">
-        <Card :parrafo="parrafo"></Card>
+        <div class="grid grid-cols-2 mx-auto text-slate-300 text-sm md:text-lg gap-2">
+            <div class="col">
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 bg-slate-700 rounded-xl p-3">
+                    <div class="col">
+                        <img class="w-1/2" src="../assets/linkedin.svg" alt="">
+                    </div>
+                    <div class="col rounded-xl w-auto items-center md:flex">
+                        <a href="">asdasd</a>
+                        <a href="">asdasd</a>
+                        <a href="">asdasd</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                right
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Card from '../components/Card.vue'
-export default{
-    name:'ContactoView',
-    components:{
+export default {
+    name: 'ContactoView',
+    components: {
         Card
     },
-    data(){
-        return{
-            parrafo:'contacto coming soon!'
+    data() {
+        return {
+            parrafo: 'contacto coming soon!'
         }
     }
 }
