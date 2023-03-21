001984eb6a34bccd45a038ccc4d2773cb9b75060
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-21 12:53:08 -0500

    mejora card contacto

--- portafoliov2/src/views/ContactoView.vue
+++ portafoliov2/src/views/ContactoView.vue
@@ -1,20 +1,18 @@
 <template>
     <div class="mx-3">
         <div class="grid grid-cols-2 mx-auto text-slate-300 text-sm md:text-lg gap-2">
-            <div class="col">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 bg-slate-700 rounded-xl p-3">
-                    <div class="col">
-                        <img class="w-1/2" src="../assets/linkedin.svg" alt="">
-                    </div>
-                    <div class="col rounded-xl w-auto items-center md:flex">
-                        <a href="">asdasd</a>
-                        <a href="">asdasd</a>
-                        <a href="">asdasd</a>
+            <div class="col ">
+                <div class="bg-gray-500 w-fit sm:inline-flex rounded-xl">
+                    <img class="m-2 w-16 sm:w-32 md:w-40" src="../assets/angular.svg" alt="">
+                    <div class=" w-fit sm:flex sm:items-center m-3">
+                        <p class="text-gray-900 text-sm sm:text-lg md:text-2xl decoration-slice underline decoration-gray-800 decoration-2">
+                            <a href="https://www.linkedin.com/in/lenin-acosta" target="_blank">Estoy en Linkedin!</a> 
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                right
+            <div class="col bg-pink-400">
+                asd432
             </div>
         </div>
     </div>
