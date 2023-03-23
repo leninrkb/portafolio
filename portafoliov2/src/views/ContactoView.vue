30ed909d83d55b1d70fd8615b5de8209cd4f56c8
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-23 12:15:57 -0500

    cardimg para mostrar imgs online

--- portafoliov2/src/views/ContactoView.vue
+++ portafoliov2/src/views/ContactoView.vue
@@ -6,14 +6,7 @@
                 <CardInfo :left="index % 2 == 0" :datos="item"></CardInfo>
             </div>
         </div>
-        <div class="bg-cover bg-center rounded-lg shadow-lg h-64 relative "
-            style="background-image: url('https://picsum.photos/640/480/?category=nature')">
-            <div class="absolute rounded-lg top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
-            <div class="relative py-10 px-8 text-center text-white">
-                <h3 class="text-gray-200 text-3xl font-bold mb-2">Card con Fondo Blur</h3>
-                <p class=" text-gray-300 text-lg">Este es un ejemplo de una card con fondo blur hecha con Tailwind CSS.</p>
-            </div>
-        </div>
+        
 
 
     </div>
