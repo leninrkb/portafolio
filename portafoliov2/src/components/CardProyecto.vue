5d3c5251f4bded37c1496627372d46cf2bfadc73
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-23 22:12:04 -0500

    efectos para cards proyectos

--- portafoliov2/src/components/CardProyecto.vue
+++ portafoliov2/src/components/CardProyecto.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="rounded-xl bg-gray-700 border-gray-700 border-2">
-        <img class="rounded-t-xl w-auto" :src="datos.img" alt="">
+    <div class="rounded-xl bg-gray-700 border-gray-700 border-2 hover:scale-110 transition duration-500">
+        <div class="relative">
+            <img class="rounded-t-xl w-auto " :src="datos.img">
+            <div class="absolute top-0 left-0 w-full h-full bg-gray-900 rounded-t-xl opacity-0 hover:opacity-50"
+                @click="habrirModal()">
+                <img class="absolute w-1/3 top-1/4 left-1/3" src="../assets/eye.svg" alt="">
+            </div>
+        </div>
         <h3 class="text-gray-400 text-base sm:text-xl  uppercase p-3">titulo del proyecto</h3>
     </div>
 </template>
@@ -8,11 +14,16 @@
 <script>
 export default {
     name: 'CardProyecto',
-    props:{
-        datos:{
+    props: {
+        datos: {
             type: Array,
             required: true
         }
-    }
+    },
+    methods: {
+        habrirModal() {
+            console.log('habriendo modal...');
+        }
+    },
 }
 </script>
