0d9b5f67dab1dcb52b0e5e8ffc9c642f7d8b3473
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-29 15:23:32 -0500

    mejora en cardDetalleProyecto y Modal

--- portafoliov2/src/components/CardDetalleProyecto.vue
+++ portafoliov2/src/components/CardDetalleProyecto.vue
@@ -1,14 +1,28 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 mt-12 mb-3 mx-3">
-        <div class="col p-3 items-center">
-            <img class=" rounded-md" :src="datos.img">
+    <!-- <div class="grid grid-cols-1 sm:grid-cols-2 mt-12 mb-3 mx-3 bg-gray-700 rounded-md">
+        <div class="col p-3 bg-orange-400 w-1/2">
+            <img class="rounded-md" :src="datos.img">
         </div>
-        <div class="col">
+        <div class="col bg-purple-500 w-1/2">
             <h3 class="text-gray-400 text-base sm:text-xl  uppercase p-3">{{datos.titulo}}</h3>
             <p class="text-bas sm:text-xl p-3 text-justify">{{datos.detalle}}</p>
             <div class="p-3">
                 <Button><a :href="datos.github" target="_blank">GitHub</a></Button>
                 <Button v-if="datos.web != 'no'" class="ml-2"><a :href="datos.web" target="_blank">Ver Web</a></Button>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="bg-gray-600 rounded-md p-1 sm:w-96">
+        <!-- <img class="rounded-md w-4/5" :src="datos.img"> -->
+        <div class=" bg-gray-800 rounded-md border-2 border-cyan-600">
+            <div class="p-3">
+                <h3 class="text-gray-400 text-base sm:text-xl  uppercase p-3">{{ datos.titulo }}</h3>
+                <p class="text-base text-gray-500 p-3 sm:text-xl text-justify">{{ datos.detalle }}</p>
+                <div class="p-3">
+                    <Button><a :href="datos.github" target="_blank">GitHub</a></Button>
+                    <Button v-if="datos.web != 'no'" class="ml-2"><a :href="datos.web" target="_blank">Ver Web</a></Button>
+                </div>
             </div>
         </div>
     </div>
@@ -17,7 +31,7 @@
 import Button from './Button.vue';
 export default {
     name: 'CardDetalleProyecto',
-    components:{
+    components: {
         Button
     },
     props: {
--- portafoliov2/src/components/Modal.vue
+++ portafoliov2/src/components/Modal.vue
@@ -1,18 +1,30 @@
 <template>
-    <div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-        <div class="bg-gray-600 rounded-lg shadow-lg overflow-hidden w-fit relative m-8">
+    <!-- <div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div class="bg-gray-600 rounded-lg shadow-lg w-fit relative mx-10 my-14">
             <img class="absolute top-0 right-0 w-10 hover:bg-red-600" src="../assets/close.svg" @click="closeModal()">
             <slot></slot>
+        </div>
+    </div> -->
+    <div class="fixed inset-0 flex items-center justify-center bg-gray-800 opacity-80"></div>
+    <div class="fixed inset-0 flex items-center justify-center">
+        <div class="grid grid-cols-1 z-50 mx-3">
+            <div class="col">
+                <div class="relative">
+                    <img class="bg-gray-600 absolute top-0 right-0 w-10 hover:bg-red-600 rounded-md" src="../assets/close.svg"
+                        @click="closeModal()">
+                    <slot></slot>
+                </div>
+            </div>
         </div>
     </div>
 </template>
   
 <script>
 export default {
-    name:'Modal',
+    name: 'Modal',
     methods: {
         closeModal() {
-            this.$emit("close",false);
+            this.$emit("close", false);
         }
     },
 };
