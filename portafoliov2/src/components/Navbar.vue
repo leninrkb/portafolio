e51074a5a035a23e4100ef2f9a6d184f243479a2
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-29 19:58:25 -0500

    arreglo navbar

--- portafoliov2/src/components/Navbar.vue
+++ portafoliov2/src/components/Navbar.vue
@@ -71,9 +71,9 @@
 
         <DisclosurePanel class="sm:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
-                <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
-                    :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+                <DisclosureButton v-for="item in navigation" :key="item.name" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"  :aria-current="item.current ? 'page' : undefined">
+                    <router-link :to="item.href">{{ item.name }}</router-link>
+                </DisclosureButton>
             </div>
         </DisclosurePanel>
     </Disclosure>
