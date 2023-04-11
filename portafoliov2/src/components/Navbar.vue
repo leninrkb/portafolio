43cf9ccee1cd8138407a5f2aa7d7b7897af98925
Lenin Acosta <rkb.lenin@gmail.com>
2023-04-10 21:22:50 -0500

    agrego magic mouse js

--- portafoliov2/src/components/Navbar.vue
+++ portafoliov2/src/components/Navbar.vue
@@ -37,7 +37,7 @@
                                 @click="go2github"
                                 class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-gray-800">
                                 <!-- <span class="sr-only">Open user menu</span> -->
-                                <img class="h-10 w-10 rounded-full bg-sky-500 border-2 border-sky-700" src="../assets/github.svg" alt="" />
+                                <img class=" h-10 w-10 rounded-full bg-sky-500 border-2 border-sky-700" src="../assets/github.svg" alt="" />
                             </MenuButton>
                         </div>
                         <!-- <transition enter-active-class="transition ease-out duration-100"
--- portafoliov2/src/main.js
+++ portafoliov2/src/main.js
@@ -4,6 +4,17 @@ import router from './router'
 import './assets/tailwind.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import magicMouse from 'magicmouse.js';
+let options = {
+    "cursorOuter": "circle-basic",
+    "hoverEffect": "circle-move",
+    "hoverItemMove": false,
+    "defaultCursor": false,
+    "outerWidth": 30,
+    "outerHeight": 30
+};
+magicMouse(options);
+
 AOS.init({
     delay: 100,
     duration: 800,
